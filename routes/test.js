a457eb721338e1227764fcdb9b265b4a3cbde2a8
YuShijiaCode <58650345+YuShijiaCode@users.noreply.github.com>
2022-04-08 15:46:51 +0800

    Create test.js

--- routes/test.js
+++ routes/test.js
@@ -1,24 +1,31 @@
 let express = require('express');
 let path = require('path');
 const httpRequest = require('../framework/httpRequest');
+const connectMysql = require('../framework/connectMysql');
 let router = express.Router();
 
 
 /* GET users listing. */
 router.get('/index', function(req,res) {
-  res.sendFile(path.join(__dirname,'/../public/javascripts/index.html'));
-  //console.log('./public/javascripts/index.html');
+  // res.sendFile("C:\\Users\\yushijia\\IdeaProjects\\APITool\\public\\javascripts\\index.html");
+  res.sendFile(path.join(__dirname,'/../public/javascripts/Login.html'));
+  // console.log('public/javascripts/index.html');
 });
 
 router.get('/returnRes', function(req, res, next) {
-//  let response  = {
-//    'firstname': req.query.firstname,
-//    'lastname': req.query.lastname
-//  }
-//  console.log('---------------------------------------------------------------------');
-//  console.log(response);
-//  res.send(JSON.stringify(response));
- res.sendFile(path.join(__dirname,'/../public/javascripts/nodejsfile.html'));
+  let response  = {
+    'username': req.query.username,
+    'password': req.query.password
+  }
+  connectMysql.select_Login_User_Info("",(results)=>{
+    if (results[0].username  == req.query.username){
+      res.send(results)
+    }else{
+      res.send("这个用户不存在")
+    }
+  });
 });
 
 module.exports = router;
+
+
